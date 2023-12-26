3db1ed9f801b664c537993baf84687f168309f53
Your Name <you@example.com>
2023-12-26 18:58:31 +0300

    Elizaveta Khanan

--- Reverse.java
+++ Reverse.java
@@ -6,10 +6,26 @@ public class Reverse {
 	public static void main (String[] args){
 		//// Put your code here
 		String s = args[0];
-		for (int j=s.length(); j>=0;--j)
+		int n = s.length();
+		int i = n-1;
+		int a = (s.length()/2)-1;
+		int b = (s.length()-1)/2;
+		
+
+		while ( i>=0)
 		{
-			System.out.println(s.charAt(j-1));
+			System.out.print(s.charAt(i));
+			i = i - 1;
 		}
+		System.out.println();
+		{
+       if (s.length() % 2 == 0)
+	   {
+		System.out.println("The middle character is " + s.charAt(a));
+	   } else {
+		System.out.println("The middle character is " + s.charAt(b));
+	   } }
+
 		}
 		
 	}
