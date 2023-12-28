bf2f5afd00657408878f104bb26fd30db8eec747
Elizaveta Khanan <154255907+Lizahanan@users.noreply.github.com>
2023-12-29 01:45:27 +0300

    InOrder.java
    Liza. commit

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,19 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
-	}
+		int a =(int)((Math.random() * (10 + 1)+0));
+		 System.out.print(a + " ");
+		
+	do {
+		 
+		 int b = (int)((Math.random() * (10 + 1)+0));
+		 if (b>=a) 
+		 System.out.print(b + " ");
+		 else break;
+		 if (a>=b)
+		 System.out.print(a + " ");
+		 else break;
+	} while (true);
+    
+}
 }
