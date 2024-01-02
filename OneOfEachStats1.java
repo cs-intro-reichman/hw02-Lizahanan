19bc1818337efd2b289103b39e61433a12619a25
Elizaveta Khanan <154255907+Lizahanan@users.noreply.github.com>
2024-01-03 01:35:48 +0300

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  *  Computes some statistics about families in which the parents decide 
  *  to have children until they have at least one child of each gender.
@@ -7,5 +9,59 @@
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
 		//// Put your code here
+	 int T = Integer.parseInt(args[0]);
+    int childSumCount = 0;
+    int twochildlren = 0;
+    int threechildren = 0;
+    int fourplus = 0;
+    for(int i = 0; i<T; i++){
+        int childrenPerFamily=0;
+        boolean boy = false;
+        boolean girl = false;
+        while (!boy || !girl){
+            double prob = Math.random();
+
+            if (prob < 0.5){
+                boy = true;
+
+            } else {
+                girl = true;
+
+            }
+            childSumCount++;
+            childrenPerFamily++;
+
+        } 
+        if (childrenPerFamily==2) {
+            twochildlren++;
+        }
+        else if(childrenPerFamily==3){
+            threechildren++;
+        }else if(childrenPerFamily >= 4){
+            fourplus++;
+        }
+
+    }
+    double average = (childSumCount/(double)T);
+     System.out.print("Average: "+ average+ " children to get at least one of each gender");
+     System.out.println();
+	 System.out.println("Number of families with 2 children: " + twochildlren);
+	 System.out.println("Number of families with 3 children: " + threechildren);
+	 System.out.println("Number of families with 4 or more children: " + fourplus);
+	 String mode;
+	
+	 if (twochildlren > threechildren && twochildlren > fourplus) {mode = "2";}
+	 else if (threechildren > twochildlren && threechildren > fourplus) {mode = "3";}
+	 else if (fourplus > twochildlren && fourplus > threechildren) {mode = "4 or more";}
+	 else if (twochildlren == threechildren && twochildlren > fourplus) {mode = "2";}
+	 else if (twochildlren == fourplus && twochildlren > threechildren) {mode = "2";}
+	 else if (threechildren == fourplus && threechildren> twochildlren) {mode = "3";}
+	 else {mode = "4 or more";}
+	 
+
+	 System.out.println("The most common number of children is " + mode + ".");
+
 	}
 }
+	
+
